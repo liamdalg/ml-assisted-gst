e6d3dfc850ead3729d56f2a8ec8a6c09ea1798ea
Liam Dalgarno <liamdalg99@gmail.com>
2021-03-04 05:25:32 +0000

    Generate particles for SMC

--- code/heaters.py
+++ code/heaters.py
@@ -24,11 +24,23 @@ def orthogonal_vector(vec: np.ndarray) -> np.ndarray:
     return orth(np.column_stack((vec, rand_vec)))[:, 1]
 
 
+def init_c(
+    heaters: int, diag_mean: float, diag_sd: float, corr_low: float, corr_high: float
+) -> np.ndarray:
+    """Initialise a random C matrix where the diagonal is sampled from `N(diag_mean, diag_sd)`
+    and the off-diagonal is sampled from `U(corr_interval)`."""
+
+    c_matrix = rng.uniform(corr_low, corr_high, (heaters, heaters))
+    c_matrix[np.diag_indices_from(c_matrix)] = rng.normal(diag_mean, diag_sd, heaters)
+    return c_matrix
+
+
+# TODO: put data loading + preparation into functions or other file
 data_path = r'BayesianXTalk5heaters2018-03-10 17-27-02.h5'
 with h5py.File(data_path, 'r') as h5_data:
     input_states = h5_data['InputStates'][:]
     output_states = h5_data['OutputStates'][:]
-    power_h5 = h5_data['Powers'][:, 2:-2].flatten()
+    power_h5 = h5_data['Powers'][:, 2:-2]
     exp_counts = h5_data['ExpCounts'][:]
 
 prob_0 = exp_counts[:, 0] / np.sum(exp_counts, axis=1)
@@ -40,3 +52,23 @@ for i, state in enumerate(out_states):
         out_states[i] = output_states[i]
     else:
         out_states[i] = orthogonal_vector(output_states[i])
+
+
+# TODO: put prior generation in function too? probably should move all of the experiment constants
+#   up to the top aswell
+heaters = power_h5.shape[1]
+particle_count = 300
+resample_threshold = 0.4
+a = 0.9  # a is a parameter for the Liu-West resampler
+
+power_interval = (0., 50.)
+c_diag_mean = 2 * np.pi / max(power_interval)
+c_diag_sd = 0.3 * c_diag_mean
+corr_interval = (-0.2 * c_diag_mean, 0.2 * c_diag_mean)
+
+# generate a list of particles, where each particle is (C, ϕ)
+particles = [
+    (init_c(heaters, c_diag_mean, c_diag_sd, *corr_interval), rng.uniform(0, 2 * np.pi, heaters))
+    for _ in range(particle_count)
+]
+weights = np.full((particle_count,), 1.0 / particle_count)
