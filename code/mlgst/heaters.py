1b580930bceeb674af9c3feb4b80d784c7f2fa30
Liam Dalgarno <liamdalg99@gmail.com>
2021-03-08 15:49:49 +0000

    WIP: parameter estimation is almost there, but unstable

--- code/mlgst/heaters.py
+++ code/mlgst/heaters.py
@@ -3,7 +3,9 @@ from typing import List, Tuple
 import h5py
 import numpy as np
 
+from matplotlib import pyplot as plt
 from numpy.random import default_rng
+from numpy import linalg
 from scipy.linalg import orth
 from tqdm import tqdm
 
@@ -26,7 +28,9 @@ def random_vector(dim: int) -> np.ndarray:
 def orthogonal_vector(vec: np.ndarray) -> np.ndarray:
     """Generate a random vector which is orthogonal to `vec`."""
     rand_vec = random_vector(vec.shape[0])
-    return orth(np.column_stack((vec, rand_vec)))[:, 1]
+    mat = np.column_stack((vec, rand_vec))
+    q, _ = linalg.qr(mat)
+    return q[:, 1]
 
 
 def init_c(
@@ -41,9 +45,15 @@ def init_c(
 
 def mean_cov(arr: np.ndarray, weights: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """Calculate the mean and co-variance of `arr`, weighted by `weights`."""
-    mean = np.sum(weights * arr, axis=0)
-    cov = np.sum(weights[:, :, np.newaxis] @ (arr[:, :, np.newaxis] @ arr[:, np.newaxis, :]), axis=0) - (mean[:, np.newaxis] * mean)
+    mean = np.sum(weights[:, np.newaxis] * arr, axis=0)
+    cov = np.sum(weights[:, np.newaxis, np.newaxis] * (arr[:, :, np.newaxis] @ arr[:, np.newaxis, :]), axis=0) - (mean[:, np.newaxis] * mean)
     return mean, cov
+
+
+def cov_norm(arr: np.ndarray, weights: np.ndarray) -> float:
+    _, cov = mean_cov(arr.reshape((arr.shape[0], -1)), weights)
+    eig = linalg.eigvals(cov)
+    return linalg.norm(eig)
 
 
 def likelihood(
@@ -53,21 +63,32 @@ def likelihood(
     """Calculate the likelihood |⟨D|U(Cx)|Ψ⟩|², where U(Cx) is the unitary operator associated with
     `powers`, `c_mat`, and `phi0`."""
     unitary = get_unitary(powers, c_mat, phi0)
-    return np.abs(np.conjugate(output_state) @ unitary @ input_state) ** 2
+    result = np.abs(np.conjugate(output_state) @ unitary @ input_state) ** 2
+    return result
 
 
-def liu_west_resample(particles: np.ndarray, weights: np.ndarray, a: float) -> np.ndarray:
+def liu_west_resample(
+    particles_c: np.ndarray, particles_phi: np.ndarray, weights: np.ndarray, a: float
+) -> np.ndarray:
     """Resample `particles` using the Liu and West (2001) resampler."""
-    flat_particles = particles.reshape((particles.shape[0], -1))
+    flat_particles = np.concatenate((particles_c.reshape((particles_c.shape[0], -1)), particles_phi), axis=1)
     mean, cov = mean_cov(flat_particles, weights)
     cov *= (1 - (a ** 2))
 
     resample_means = (a * flat_particles) + ((1 - a) * mean)
     new_particles = np.zeros_like(flat_particles)
-    for i in range(weights.shape[0]):
-        new_particles[i] = rng.multivariate_normal(resample_means[i], cov)
+    for i in range(particle_count):
+        rand = rng.random()
+        count = 0
+        for j in range(particle_count):
+            count += weights[j]
+            if count > rand:
+                break
+        new_particles[i] = rng.multivariate_normal(resample_means[j], cov)
 
-    return new_particles.reshape(particles.shape)
+    heaters = particles_phi.shape[1]
+    new_c, new_phi = np.split(new_particles, [heaters * heaters], axis=1)
+    return new_c.reshape((particle_count, heaters, heaters)), new_phi
 
 
 def update_estimate(
@@ -92,19 +113,19 @@ def update_estimate(
     Returns:
         Updated particles for C and phi, and the new particle weights.
     """
-    new_weights = np.zeros_like(weights)
     for i in range(weights.shape[0]):
-        new_weights[i, :] = weights[i] * likelihood(
+        weights[i] = weights[i] * likelihood(
             input_state, output_state, powers, particles_c[i], particles_phi[i]
         )
-    new_weights /= np.sum(new_weights)
+    weights /= np.sum(weights)
 
-    if np.sum(new_weights ** 2) > 1 / (resample_threshold * weights.shape[0]):
-        particles_c = liu_west_resample(particles_c, weights, resample_a)
-        particles_phi = liu_west_resample(particles_phi, weights, resample_a)
-        new_weights.fill(1.0 / new_weights.shape[0])
+    if np.sum(weights ** 2) > 1 / (resample_threshold * weights.shape[0]):
+        particles_c, particles_phi = liu_west_resample(
+            particles_c, particles_phi, weights, resample_a,
+        )
+        weights.fill(1.0 / weights.shape[0])
 
-    return particles_c, particles_phi, new_weights
+    return particles_c, particles_phi, weights
 
 
 # TODO: put data loading + preparation into functions or other file
@@ -117,12 +138,9 @@ with h5py.File(data_path, 'r') as h5_data:
 
 prob_0 = exp_counts[:, 0] / np.sum(exp_counts, axis=1)
 
-out_states = np.zeros_like(output_states)
-for i, state in enumerate(out_states):
-    if prob_0[i] > rng.random():
-        out_states[i] = output_states[i]
-    else:
-        out_states[i] = orthogonal_vector(output_states[i])
+for i in range(output_states.shape[0]):
+    if prob_0[i] < rng.random():
+        output_states[i] = orthogonal_vector(output_states[i])
 
 # TODO: put prior generation in function too? probably should move all of the experiment constants
 #   up to the top aswell
@@ -140,18 +158,25 @@ corr_interval = (-0.2 * c_diag_mean, 0.2 * c_diag_mean)
 # right now they're stored as 3d stacked matrices, where each matrix is a single particle
 # it requires some funky reshaping in the mean_cov function to get it to work properly
 # should probably just simplify it and reshape as necessary :\
-particles_c = np.zeros((particle_count, heaters, heaters, ))
+particles_c = np.zeros((particle_count, heaters, heaters))
 particles_phi = np.zeros((particle_count, heaters))
-weights = np.full(particle_count, 1.0 / particle_count).reshape((-1, 1))
+weights = np.full(particle_count, 1.0 / particle_count)
 for i in range(particle_count):
     particles_c[i, :, :] = init_c(heaters, c_diag_mean, c_diag_sd, *corr_interval)
     particles_phi[i, :] = rng.uniform(0, 2 * np.pi, heaters)
 
 # update all parameters, and construct a list of the weight + particles at each stage
 particles_evolution = []
+cov_evolution = []
 for i in tqdm(range(data_length)):
     particles_c, particles_phi, weights = update_estimate(
         particles_c, particles_phi, weights, input_states[i], output_states[i], all_powers[i],
         resample_threshold, resample_a
     )
     particles_evolution.append((particles_c, particles_phi, weights))
+
+final_c, final_phi, final_weights = particles_evolution[-1]
+flat_particles = np.concatenate((final_c.reshape((particle_count, -1)), final_phi), axis=1)
+mean, cov = mean_cov(flat_particles, final_weights)
+print(mean)
+print(cov)
